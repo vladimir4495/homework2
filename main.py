5d7f3843014f2d967393faa5d8648a0680e2c99e
vladimir4495 <dudnichenkovova@gmail.com>
2021-04-24 16:40:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+# Task 1
+name = "Volodymyr"
+day = "Friday"
+print("Good day" + ' ' + (name) + "!" + ' ' + (day) + ' ' +"is a perfect day to learn some python.")
+
+#Task 2
+my_name = "Volodymyr"
+my_surname = "Dudnuchenko"
+print(my_name + ' ' + my_surname)
+
+#Task 3
+
+print(98 + 7)
+print(35 - 5)
+print(4 / 2)
+print(type(2 / 2))
+print(2 * 2)
+print(11 ** 5)
+print(13 // 8)
+print(type(12 // 9))
+print(9 % 8)
+print(type(12 % 6))
+print(int(5.8))
+print(float(43))
+
+"000012 Василий 110110 32.10"
+print("{0} , {1}, {2}, {3}".format(12, "Василий", 110110, 32.10))
+
+
+
+message = "Hello beautiful world!"
+final_message = message[0:5] + ' ' + message[-6:] + " Today is a " + message[6:15] + " day."
+print(final_message)
+
+
